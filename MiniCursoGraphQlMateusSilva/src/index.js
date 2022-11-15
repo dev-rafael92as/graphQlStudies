b960f9c9b5d0bf8ee2f035a92ba963d621bcd060
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-11-15 16:38:57 -0300

    [feat] Updated types and mutations on graphQl exemple

--- MiniCursoGraphQlMateusSilva/src/index.js
+++ MiniCursoGraphQlMateusSilva/src/index.js
@@ -7,17 +7,62 @@ const { ApolloServer, gql } = require('apollo-server');
 // Mutation => Manipular informações (POST/PUT/DELETE/PATCH)
 // Scalar Types => String, Int, Boolean, Float, ID
 
+
 const typeDefs = gql `
-    type Query = {
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        author: User!
+    }
+
+    type Query {
         hello: String
+        users: [User!]!
+        getUserByEmail(email: String!): User!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!): User!
     }
 `;
 
+const users = [
+    { _id: String(Math.random()), name: "Rafael A", email: "rafaela@email.com", active: true},
+    { _id: String(Math.random()), name: "Rafael B", email: "rafaelb@email.com", active: false},
+    { _id: String(Math.random()), name: "Rafael C", email: "rafaelc@email.com", active: true},
+];
+
 const resolvers = { 
     Query: {
-        hello: () => "Hello, world!"
+        hello: () => "H☺ello, world!",
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find((user) => user.email === args.email)
+        },
+    },
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                _id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true,
+            }
+            users.push(newUser)
+            return newUser
+        }
     }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => console.log(`Server rodando na ${url}`))
+
+
